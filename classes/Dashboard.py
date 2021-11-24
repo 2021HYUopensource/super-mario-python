0ab34df1bcafb0731d3b4e9a43d2946b37123781
K1A2 <jckim0307@gmail.com>
2021-11-24 20:24:03 +0900

    [DOCS] Spritesheet.py, Font.py, Dashboard.py에 docstring 추가

--- classes/Dashboard.py
+++ classes/Dashboard.py
@@ -4,7 +4,16 @@ from classes.Font import Font
 
 
 class Dashboard(Font):
-    def __init__(self, filePath, size, screen):
+    '''
+    전체적인 화면 출력에 관여하는 클래스. :class:`Font` 의 하위 클래스
+
+    :ivar filePath: 텍스트 파일 경로
+    :ivar size: 텍스트 size (안 쓰이는 파라미터)
+    :ivar screen: 출력할 화면
+    '''
+    def __init__(self, filePath:str,
+                 size:int,
+                 screen:pygame.Surface):
         Font.__init__(self, filePath, size)
         self.state = "menu"
         self.screen = screen
@@ -15,6 +24,7 @@ class Dashboard(Font):
         self.time = 0
 
     def update(self):
+        '''화면을 업데이트 하는 함수'''
         self.drawText("MARIO", 50, 20, 15)
         self.drawText(self.pointString(), 50, 37, 15)
 
@@ -33,7 +43,22 @@ class Dashboard(Font):
             self.ticks = 0
             self.time += 1
 
-    def drawText(self, text, x, y, size):
+    def drawText(self, text:str,
+                 x:int,
+                 y:int,
+                 size:int):
+        '''
+        화면에 글자를 그리는 함수
+
+        :param text: 출력할 텍스트
+        :type text: str
+        :param x: 출력할 가로 위치
+        :type x: int
+        :param y: 출력할 세로 위치
+        :type y: int
+        :param size: 출력할 텍스트 크기
+        :type size: int
+        '''
         for char in text:
             charSprite = pygame.transform.scale(self.charSprites[char], (size, size))
             self.screen.blit(charSprite, (x, y))
@@ -42,11 +67,23 @@ class Dashboard(Font):
             else:
                 x += size
 
-    def coinString(self):
+    def coinString(self) -> str:
+        '''
+        :return: 코인 출력 포멧
+        :rtype: str
+        '''
         return "{:02d}".format(self.coins)
 
-    def pointString(self):
+    def pointString(self) -> str:
+        '''
+        :return: 점수 출력 포멧
+        :rtype: str
+        '''
         return "{:06d}".format(self.points)
 
-    def timeString(self):
+    def timeString(self) -> str:
+        '''
+        :return: 남은 시간 출력 포멧
+        :rtype: str
+        '''
         return "{:03d}".format(self.time)
--- classes/Spritesheet.py
+++ classes/Spritesheet.py
@@ -2,6 +2,11 @@ import pygame
 
 
 class Spritesheet(object):
+    '''
+    스프라이트를 로딩하는 클래스
+
+    :ivar filePath: 텍스트 파일 경로
+    '''
     def __init__(self, filename):
         try:
             self.sheet = pygame.image.load(filename)
@@ -14,6 +19,23 @@ class Spritesheet(object):
 
     def image_at(self, x, y, scalingfactor, colorkey=None, ignoreTileSize=False,
                  xTileSize=16, yTileSize=16):
+        '''
+        스프라이트 이미지를 하나씩 잘라서 리턴하는 함수
+
+        :param x: 가져올 스프라이트 가로 픽셀 위치
+        :type x: int
+        :param y: 가져올 스프라이트 세로 픽셀 위치
+        :type y: int
+        :param scalingfactor: 이미지 스케일 변경 배수
+        :type scalingfactor: int
+        :param colorkey: 투명하게 만들 뒷 배경 컬러
+        :type colorkey: list[int, int, int]
+        :param ignoreTileSize: 타이틀 무시 여부
+        :type ignoreTileSize: bool
+        :param xTileSize: 타이틀 가로 사이즈. ignoreTileSize=True일 경우, backround에서 사용 될 스프라이트의 반봇 횟수로도 쓰임
+        :param yTileSize: 타이틀 세로 사이즈. ignoreTileSize=True일 경우, backround에서 사용 될 스프라이트의 반봇 횟수로도 쓰임
+        :return: scale된 이미지 리턴
+        '''
         if ignoreTileSize:
             rect = pygame.Rect((x, y, xTileSize, yTileSize))
         else:
