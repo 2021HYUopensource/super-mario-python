9919ae7916e5772e8a196e4475da003e0206de81
CheshireZzz <subinwows@gmail.com>
2021-11-29 11:32:20 +0900

    [DOCS] Coin.py 설명 중간 저장 및 작업 시작 #1

--- entities/Coin.py
+++ entities/Coin.py
@@ -4,6 +4,9 @@ from entities.EntityBase import EntityBase
 
 
 class Coin(EntityBase):
+    '''
+    코인 엔티티에 관한 클래스
+    '''
     def __init__(self, screen, spriteCollection, x, y, gravity=0):
         super(Coin, self).__init__(x, y, gravity)
         self.screen = screen
